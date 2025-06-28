f9680fd60cb5598476f6e7e15712896b4c2fe8ba
shafi <salekin68@gmail.com>
2025-06-28 15:58:16 +0600

    feat: about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import PlaySVG from "../../public/logo/Play.svg";
+import Icon1SVG from "../../public/image/about-img-1.svg";
+import Icon2SVG from "../../public/image/about-img-2.svg";
+import Icon3SVG from "../../public/image/about-img-3.svg";
+import Icon4SVG from "../../public/image/about-img-4.svg";
 
 const About = () => {
   return (
-    <div className='w-full max-w-[1440px] mx-auto flex items-center justify-center md:mt-[162px] md:mb-[80px]'>
-      <h2 className='text-[64px] font-aeonik font-medium leading-[65px] text-white'>About</h2>
+    <div className="w-full md:mt-[162px] md:mb-[80px]">
+      <div className="flex flex-row gap-4 items-start justify-start overflow-x-auto scrollbar-hide">
+        <img
+          src={Icon1SVG}
+          alt="about image"
+          className="w-[351px] md:w-[511.46px] h-[226px] md:h-[329px] flex-shrink-0"
+        />
+        <img
+          src={Icon2SVG}
+          alt="about image"
+          className="w-[277.28px] h-[226px] md:h-[329px] flex-shrink-0"
+        />
+        <img
+          src={Icon3SVG}
+          alt="about image"
+          className="w-[277.28px] h-[226px] md:h-[329px] flex-shrink-0"
+        />
+        <img
+          src={Icon4SVG}
+          alt="about image"
+          className="w-[474.82px] h-[226px] md:h-[329px] flex-shrink-0"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/CoverSection.tsx
+++ src/components/CoverSection.tsx
@@ -31,7 +31,7 @@ const CoverSection = () => {
         {/* Black Linear Gradient Overlay */}
         <div className="absolute inset-0 bg-gradient-to-b from-black/0 via-black/10 to-black/40"></div>
         {/* Content */}
-        <div className="w-full flex items-center justify-center mt-[317px] mb-10 md:mb-15 z-20">
+        <div className="w-full flex items-center justify-center mt-[476px] mb-10 md:mb-15 z-20">
           <CoverInfoOverlay />
         </div>
       </section>
