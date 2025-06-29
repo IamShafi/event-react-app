fab5d1ff3a7026018b413612e7aec1064ad62526
shafi <salekin68@gmail.com>
2025-06-29 19:40:04 +0600

    fix: footer

---
+++ src/components/BookingSuccess/CoverInfoOverlay.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoverInfoOverlay = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CoverInfoOverlay
---
+++ src/components/BookingSuccess/CoverSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoverSection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CoverSection
